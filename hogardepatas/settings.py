c7b0a5ae10bc9afbad33c8a7b84f1b052f51032d
jota <juan.erices04@inacapmail.cl>
2024-10-21 16:31:11 -0300

    Refactor faq_page.html to update language code to Spanish and improve FAQ section

--- hogardepatas/settings.py
+++ hogardepatas/settings.py
@@ -27,6 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['hogardepatas-e0b0b92bd186.herokuapp.com','127.0.0.1'] # '127.0.0.1' es para que runserver funcione, osea una ip local
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'hogarde4patas@gmail.com'  # Coloca tu dirección de correo
+EMAIL_HOST_PASSWORD = 'hogardepatas444'     # Coloca tu contraseña
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Application definition
 
